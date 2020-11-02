234d2c47893b1c2b794e04465fc145e7cedc0e45
Daniel Kvasnicka <daniel.kvasnicka@hey.com>
2020-11-02 12:31:26 +0100

    Drobna optimalizace procesu hledani dalsi hrany

--- fleury.rkt
+++ fleury.rkt
@@ -31,21 +31,26 @@
     (remove-edge! new-graph u v)
     new-graph))
 
-(struct maybe-step (vertex next-graph ccs))
+(struct step (vertex graph ccs))
+
 (define/match (candidate-comparator x y)
-  [((maybe-step _ _ xcs) (maybe-step _ _ ycs)) (< (length xcs) (length ycs))])
+  [((step _ _ xcs) (step _ _ ycs)) (< (length xcs) (length ycs))])
+
+(define (make-step G start v)
+  (let ([without-edge (remove-edge G start v)])
+    (step v without-edge (cc without-edge))))
 
 (define (edge-candidates G start)
   (for/fold ([acc (heap candidate-comparator)])
-            ([v (in-neighbors G start)])
-    (let ([without-edge (remove-edge G start v)])
-      (insert (maybe-step v without-edge (cc without-edge)) acc))))
+            ([s (sequence-map (curry make-step G start) (in-neighbors G start))]
+             #:final (= 1 (length (step-ccs s))))
+    (insert s acc)))
 
 (define (traverse G start)
   (if (zero? (length (get-edges G)))
       '()
-      (let ([next-step (find-min/max (edge-candidates G start))])
-        (cons (list start (maybe-step-vertex next-step))
-              (traverse (maybe-step-next-graph next-step) (maybe-step-vertex next-step))))))
+      (let ([next (find-min/max (edge-candidates G start))])
+        (cons (list start (step-vertex next))
+              (traverse (step-graph next) (step-vertex next))))))
 
 (traverse *g* 'c)
